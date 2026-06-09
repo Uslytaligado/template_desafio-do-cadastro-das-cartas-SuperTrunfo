ec35d80e92f4bf89d25800812c54b7077f0374e1
Uslytaligado <159175924+Uslytaligado@users.noreply.github.com>
2026-06-09 20:44:19 +0000

    densidade e percapita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   char estado1[2], estado2[2], cdgCarta1[4],cdgCarta2[4], cidade1[50], cidade2[50];
-  float populacion1, populacion2, area1, area2, PIB1, PIB2;
+  float populacion1, populacion2, percapita1, percapita2, area1, area2, PIB1, PIB2, densidade1, densidade2;
   int turismo1, turismo2;
   // Área para entrada de dados
   printf("Digite uma UF de estado: \n");
@@ -22,6 +22,8 @@ int main() {
   scanf("%f", &PIB1);
   printf("Quantos pontos turisticos tem sua cidade: \n");
   scanf("%d", &turismo1);
+  densidade1 = populacion1 / area1;
+  percapita1 = populacion1 / PIB1;
 
   printf("Digite uma UF de estado: \n");
   scanf("%s", &estado2);
@@ -35,8 +37,10 @@ int main() {
   scanf("%f", &PIB2);
   printf("Quantos pontos turisticos tem sua cidade: \n");
   scanf("%d", &turismo2);
+  densidade2 = populacion2 / area2;
+  percapita2 = populacion2 / PIB2;
 
-  // Área para exibição dos dados da cidade
+  // Área para exibiçã o dos dados da cidade
   printf("Carta 1 \n");
   printf("O estado é:%s \n", estado1);
   printf("O codigo é %s01 \n",estado1);
@@ -45,6 +49,8 @@ int main() {
   printf("A area da cidade é de: %f \n", area1);
   printf("O PIB é de: %f \n", PIB1);
   printf("Existem %d pontos turisticos! \n", turismo1);
+  printf("A densidade da cidade é de: %2.f \n", densidade1);
+  printf("O PIB per capita é de : %f \n", percapita1);
 
   printf("Carta 2 \n");
   printf("O estado é:%s \n", estado2);
@@ -54,6 +60,7 @@ int main() {
   printf("A area da cidade é de: %f \n", area2);
   printf("O PIB é de: %f \n", PIB2);
   printf("Existem %d pontos turisticos! \n", turismo2);
-
+  printf("A densidade da cidade é de: %2.f \n", densidade2);
+  printf("O PIB per capita é de : %f \n", percapita2);
 return 0;
 } 
